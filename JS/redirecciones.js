e0c8b414cc58cd1486bf2c0f187d76a529fbe5c1
JulianSilva-coder <jsilvabe@unbosque.edu.co>
2022-06-08 01:55:05 -0500

    registro login y admin 65%

---
+++ JS/redirecciones.js
@@ -0,0 +1,9 @@
+/*redireccion de registro a principal HTML*/
+function volver(){
+    location.href = "Ventana_Principal.html";
+}
+
+/*redireccion de Cerrar Sesion a principal HTML*/
+function cerrarsesion(){
+    location.href = "Ventana_Principal.html";
+}
