2f296c5e5b29e73dffd8244b2be47e6cfd23593e
nicko1890 <nico18b@yahoo.es>
2022-06-10 00:16:29 -0500

    actual

--- JS/redirecciones.js
+++ JS/redirecciones.js
@@ -7,3 +7,19 @@ function volver(){
 function cerrarsesion(){
     location.href = "Ventana_Principal.html";
 }
+
+/*redireccion de EditCliente a Cliente HTML*/
+function volver(){
+    location.href = "Cliente.html";
+}
+
+/*redireccion de Cliente a EditCliente*/
+function editPerfil(){
+    location.href = "EditCliente.html";
+}
+
+/*redireccion de Cliente a VentanaPrincipal HTML*/
+function cerrarSe(){
+    location.href = "Ventana_Principal.html";
+}
+
